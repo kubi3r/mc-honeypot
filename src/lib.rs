c212995c88d45c8ae7dce0d96d8f07497781536b
straw <98976190+kubi3r@users.noreply.github.com>
2025-10-25 18:13:13 +0300

    switch to using async so we can have concurrency

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
-use std::io::{Bytes, Error, Read};
+use tokio::io::{Error, AsyncRead, AsyncReadExt};
 
-pub fn read_varint<T>(byte_iter: &mut Bytes<T>) -> Result<(i32, i32), Error>
-where T: Read
-{
+pub async fn read_varint<T: AsyncRead + Unpin>(byte_iter: &mut T) -> Result<(i32, i32), Error> {
     let mut value: i32 = 0;
     let mut bytes_read = 0;
 
     loop {
-        let current_byte = match byte_iter.next() {
-            Some(byte) => byte?,
-            None => return Err(Error::other("iterator ended while reading varint"))
-        };
+        let current_byte = byte_iter.read_u8().await?;
         
         value |= ((current_byte & 0x7F) as i32) << (bytes_read * 7);
 
@@ -45,20 +40,15 @@ pub fn write_varint(num: i32) -> Vec<u8> {
 }
 
 
-pub fn read_string<T>(byte_iter: &mut Bytes<T>) -> Result<(String, i32), Error>
-where T: Read
-{   
+pub async fn read_string<T: AsyncRead + Unpin>(byte_iter: &mut T) -> Result<(String, i32), Error> {   
     let mut output = String::new();
     let mut total_bytes_consumed = 0;
 
-    let (string_len, bytes_consumed) = read_varint(byte_iter)?;
+    let (string_len, bytes_consumed) = read_varint(byte_iter).await?;
     total_bytes_consumed += bytes_consumed;
 
     for _ in 0..string_len {
-        let current_byte = match byte_iter.next() {
-            Some(byte) => byte?,
-            None => return Err(Error::other("iterator ended while reading string"))
-        };
+        let current_byte = byte_iter.read_u8().await?;
         total_bytes_consumed += 1;
 
         output.push(current_byte as char);
@@ -71,10 +61,10 @@ pub fn prefix_str_len(string: &str) -> Vec<u8> {
     [&write_varint(string.len().try_into().unwrap()), string.as_bytes()].concat()
 }
 
-pub fn send_webhook(message: String, webhook_url: &str) -> Result<(), reqwest::Error> {
-    let client = reqwest::blocking::Client::new();
+pub async fn send_webhook(message: String, webhook_url: &str) -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::new();
     client.post(webhook_url)
         .json(&serde_json::json!({"content": message}))
-        .send()?;
+        .send().await?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::fs;
-use std::io::{Bytes, Error, Read, Write};
-use std::net::{TcpListener, TcpStream};
-use base64::Engine;
 use mc_honeypot::{read_varint, write_varint, read_string, prefix_str_len, send_webhook};
+use base64::Engine;
 use serde_json::{json, Value};
 use uuid::Uuid;
 use clap::Parser;
 use base64::{engine::general_purpose};
+use tokio::{fs, net::{TcpListener, TcpStream}, io::{AsyncReadExt, AsyncWriteExt, AsyncRead, Error}};
+use std::io::Cursor;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -19,57 +18,56 @@ struct Args {
     server_icon: Option<String>
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
     let mut server_icon = None;
     if let Some(ref icon_path) = args.server_icon {
-        let image_bytes = fs::read(icon_path).unwrap_or_else(|_| panic!("couldn't find image at {icon_path}"));
+        let image_bytes = fs::read(icon_path).await.unwrap_or_else(|_| panic!("couldn't find image at {icon_path}"));
         let base64_encoded = general_purpose::STANDARD.encode(image_bytes);
 
         server_icon = Some(format!("data:image/png;base64,{base64_encoded}"));
     }
 
-    let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:25565").await.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                if let Ok(message) = handle_connection(stream, &server_icon) {
-                    println!("{message}");
+        let webhook = args.webhook.clone();
+        let icon = server_icon.clone();
 
-                    if let Some(webhook) = &args.webhook {
-                        if let Err(e) = send_webhook(message, webhook) {
-                            println!("failed to send messsage to webhook: {e}")
-                        }
-                    }
+        tokio::spawn(async move {
+            if let Ok(message) = handle_connection(stream, icon).await {
+                println!("{message}");
+
+                if let Some(webhook) = webhook
+                    && let Err(e) = send_webhook(message, &webhook).await {
+                        println!("failed to send messsage to webhook: {e}")
                 }
-
-            },
-            Err(e) => println!("connection error: {e}")
-        }
+            }
+        });
     }
 }
 
-fn handle_connection(mut stream: TcpStream, server_icon: &Option<String>) -> Result<String, Error> {
-    let (packet_id, data) = read_packet(&mut stream)?; // handshake
+async fn handle_connection(mut stream: TcpStream, server_icon: Option<String>) -> Result<String, Error> {
+    let (packet_id, data) = read_packet(&mut stream).await?; // handshake
 
     if packet_id != 0 {
         return Err(Error::other(""))
     }
-
-    match read_handshake(&mut data.bytes())? {
+    
+    match read_handshake(&mut Cursor::new(data)).await? {
         Handshake::Status => {
-            let packet_id = read_packet(&mut stream)?.0; // status request
+            let packet_id = read_packet(&mut stream).await?.0; // status request
             if packet_id != 0 {
                 return Err(Error::other(""))
             }
 
-            if send_status_resp(&mut stream, server_icon).is_ok() { // status response
-                if let Ok((packet_id, data)) = read_packet(&mut stream) { // ping request
+            if send_status_resp(&mut stream, server_icon).await.is_ok() { // status response
+                if let Ok((packet_id, data)) = read_packet(&mut stream).await { // ping request
                     if packet_id == 1 && data.len() == 8 {
-                        write_packet(&mut stream, 1, &data).unwrap_or(()); // pong
+                        write_packet(&mut stream, 1, &data).await.unwrap_or(()); // pong
                     }
                 }
             }
@@ -77,29 +75,27 @@ fn handle_connection(mut stream: TcpStream, server_icon: &Option<String>) -> Res
             Ok(format!("STATUS `{}`", stream.peer_addr()?.ip()))
         },
         Handshake::Login => {
-            let (packet_id, data) = read_packet(&mut stream)?; // login start
+            let (packet_id, data) = read_packet(&mut stream).await?; // login start
             if packet_id != 0 {
                 return Err(Error::other(""))
             }
 
-            let mut byte_iter = data.bytes();
-
-            let username = read_string(&mut byte_iter)?.0;
+            let username = read_string(&mut Cursor::new(data)).await?.0;
             let uuid = Uuid::new_v3(&Uuid::NAMESPACE_DNS, format!("OfflinePlayer:{username}").as_bytes());
 
             write_packet(&mut stream, 2, &[ // login success
                 uuid.as_bytes(),
                 &prefix_str_len(&username)[..],
                 &write_varint(0),
-            ].concat())?;
+            ].concat()).await?;
             
             Ok(format!("JOIN `{}`, username: `{username}`", stream.peer_addr()?.ip()))
         },
     }
 }
 
-fn send_status_resp(stream: &mut TcpStream, server_icon: &Option<String>) -> Result<(), Error> {
-    let status_response = fs::read_to_string("status_resp.json")
+async fn send_status_resp(stream: &mut TcpStream, server_icon: Option<String>) -> Result<(), Error> {
+    let status_response = fs::read_to_string("status_resp.json").await
         .expect("couldn't find status_resp.json");
 
     let mut parsed: Value = serde_json::from_str(&status_response).expect("failed to parse status_resp.json");
@@ -110,27 +106,25 @@ fn send_status_resp(stream: &mut TcpStream, server_icon: &Option<String>) -> Res
 
     let json_str = serde_json::to_string(&parsed).unwrap();
 
-    write_packet(stream, 0, &prefix_str_len(&json_str))?;
+    write_packet(stream, 0, &prefix_str_len(&json_str)).await?;
     Ok(())
 }
 
-fn write_packet(stream: &mut TcpStream, packet_id: i32, data: &[u8]) -> Result<(), Error> {
+async fn write_packet(stream: &mut TcpStream, packet_id: i32, data: &[u8]) -> Result<(), Error> {
     let packet_id = write_varint(packet_id);
     let packet_len = write_varint((packet_id.len() + data.len()).try_into().unwrap());
 
-    stream.write_all(&[packet_len.as_slice(), packet_id.as_slice(), data].concat())
+    stream.write_all(&[packet_len.as_slice(), packet_id.as_slice(), data].concat()).await
 }
 
-fn read_packet(stream: &mut TcpStream) -> Result<(i32, Vec<u8>), Error> {
-    let mut byte_iter = stream.bytes();
-
-    let total_packet_len = read_varint(&mut byte_iter)?.0;
-    let (packet_id, packet_id_len) = read_varint(&mut byte_iter)?;
+async fn read_packet(stream: &mut TcpStream) -> Result<(i32, Vec<u8>), Error> {
+    let total_packet_len = read_varint(stream).await?.0;
+    let (packet_id, packet_id_len) = read_varint(stream).await?;
 
     let data_len = total_packet_len - packet_id_len;
     let mut data_buf = vec![0u8; data_len.try_into().unwrap()];
 
-    stream.read_exact(&mut data_buf)?;
+    stream.read_exact(&mut data_buf).await?;
 
     Ok((packet_id, data_buf))
 }
@@ -140,17 +134,15 @@ enum Handshake {
     Login
 }
 
-fn read_handshake<T>(byte_iter: &mut Bytes<T>) -> Result<Handshake, Error>
-where T: Read
-{
-    read_varint(byte_iter)?; // protocol
-    read_string(byte_iter)?; // server address
+async fn read_handshake<T: AsyncRead + Unpin>(byte_iter: &mut T) -> Result<Handshake, Error> {
+    read_varint(byte_iter).await?; // protocol
+    read_string(byte_iter).await?; // server address
 
     for _ in 0..2 { // port
-        byte_iter.next();
+        byte_iter.read_u8().await?;
     }
 
-    let next_state = read_varint(byte_iter)?.0;
+    let next_state = read_varint(byte_iter).await?.0;
     match next_state {
         1 => Ok(Handshake::Status),
         2 => Ok(Handshake::Login),
